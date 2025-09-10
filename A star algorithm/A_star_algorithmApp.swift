f5780e22a5c8f60a0a7f030eac5a672baa5624e6
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-09-10 16:49:45 +0900

    Initial Commit

---
+++ A star algorithm/A_star_algorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_star_algorithmApp.swift
+//  A star algorithm
+//
+//  Created by cmStudent on 2025/09/10.
+//
+
+import SwiftUI
+
+@main
+struct A_star_algorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A star algorithm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A star algorithm
+//
+//  Created by cmStudent on 2025/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
